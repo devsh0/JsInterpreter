56db837305d50788b6af2c77ef8bfc58de0692f1
devsh0 <devashishjaiswal86@gmail.com>
2022-02-14 15:23:56 +0530

    Introduce a common LoopStatement and make WhileStatement extend from it
    This is in preparation for introducing for loops. Execution of loop
    bodies in for and while are identical, so we also offload the execution
    logic of the while loop to LoopStatement.execute.

---
+++ src/ast/LoopStatement.java
@@ -0,0 +1,63 @@
+package ast;
+
+import ast.value.JSValue;
+import org.js.Interpreter;
+
+import java.util.Objects;
+
+public abstract class LoopStatement extends CompoundStatement {
+    protected Identifier label;
+    protected Expression conditionExpression;
+
+    public LoopStatement() {
+        this.conditionExpression = JSValue.from(true);
+    }
+
+    @Override
+    public Object execute() {
+        if (label != null)
+            Interpreter.get().getCurrentScope().addOrUpdateEntry(label, this);
+        Object result = JSValue.undefined();
+        while (true) {
+            var valueOrError = conditionExpression.execute();
+            VERIFY(valueOrError instanceof JSValue);
+            var value = (JSValue) valueOrError;
+            if (value.isFalsy())
+                break;
+
+            try {
+                result = body.execute();
+            } catch (BreakException exception) {
+                if (this != exception.getTarget())
+                    throw exception;
+                break;
+            } catch (ContinueException exception) {
+                if (this != exception.getTarget())
+                    throw exception;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public LoopStatement setBody(Block body) {
+        Objects.requireNonNull(body);
+        this.body = body;
+        return this;
+    }
+
+    public CompoundStatement setLabel(Identifier label) {
+        this.label = label;
+        return this;
+    }
+
+    public CompoundStatement setConditionExpression(Expression expression) {
+        if (expression != null)
+            conditionExpression = expression;
+        return this;
+    }
+
+    public Identifier getLabel() {
+        return label;
+    }
+}
--- src/ast/WhileStatement.java
+++ src/ast/WhileStatement.java
@@ -1,64 +1,19 @@
 package ast;
 
-import ast.value.JSValue;
-import org.js.Interpreter;
-
-import java.util.Objects;
-
-public class WhileStatement extends CompoundStatement {
+public class WhileStatement extends LoopStatement {
     @Override
     public Object execute() {
-        var interpreter = Interpreter.get();
-        if (label != null)
-            interpreter.getCurrentScope().addOrUpdateEntry(label, this);
-        Object result = JSValue.undefined();
-
-        while (true) {
-            var valueOrError = conditionExpression.execute();
-            ASSERT(valueOrError instanceof JSValue);
-            var value = (JSValue) valueOrError;
-            if (value.isFalsy())
-                break;
-
-            try {
-                result = body.execute();
-            } catch (BreakException exception) {
-                if (this != exception.getTarget())
-                    throw exception;
-                break;
-            } catch (ContinueException exception) {
-                if (this != exception.getTarget())
-                    throw exception;
-
-                // continue; but redundant to specify.
-            }
-        }
-        return result;
+        return super.execute();
     }
 
     @Override
     public String getDump(int indent) {
         var builder = getIndentedBuilder(indent);
-        builder.append("while (").append(conditionExpression.getDump(indent)).append(")");
+        builder.append("while (");
+        if (conditionExpression != null)
+            builder.append(conditionExpression.getDump(0));
+        builder.append(")");
         builder.append(body.getDump(indent));
         return builder.toString();
     }
-
-    public WhileStatement setConditionExpression(Expression expression) {
-        Objects.requireNonNull(expression);
-        conditionExpression = expression;
-        return this;
-    }
-
-    public CompoundStatement setLabel(Identifier label) {
-        this.label = label;
-        return this;
-    }
-
-    public Identifier getLabel() {
-        return label;
-    }
-
-    private Expression conditionExpression;
-    private Identifier label;
 }
