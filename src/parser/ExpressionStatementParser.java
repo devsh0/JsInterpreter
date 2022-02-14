1989c9383899017cf66dcb2dc941b8c12404029e
devsh0 <devashishjaiswal86@gmail.com>
2022-02-14 15:57:01 +0530

    Make ExpressionStatementParser accept empty expressions

--- src/parser/ExpressionStatementParser.java
+++ src/parser/ExpressionStatementParser.java
@@ -6,7 +6,8 @@ public class ExpressionStatementParser extends Parser {
     @Override
     public ExpressionStatement parse() {
         var statement = new ExpressionStatement();
-        statement.setSource(new ExpressionParser().parse());
+        if (!stream().peekNextToken().getValue().equals(";"))
+            statement.setSource(new ExpressionParser().parse());
         stream().consumeSemiColon();
         return statement;
     }
