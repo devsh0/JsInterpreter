b6d86b684fdecc37a6fa7068be2ac7f850803d8d
devsh0 <devashishjaiswal86@gmail.com>
2022-08-24 13:52:08 +0530

    Clean up WhileStatementParser

--- src/main/java/parser/WhileStatementParser.java
+++ src/main/java/parser/WhileStatementParser.java
@@ -1,42 +1,39 @@
 package parser;
 
+import ast.Block;
 import ast.Identifier;
 import ast.WhileStatement;
 import lexer.TokenStream;
 
 public class WhileStatementParser extends Parser {
     private Identifier label;
+
     public WhileStatementParser(TokenStream stream, ScopeManager scopeManager, Identifier label) {
         super(stream, scopeManager);
         this.label = label;
+    }
+
+    private Block parseBody(WhileStatement whileStatement) {
+        var hasBlock = stream().peekNextToken().getValue().equals("{");
+        if (hasBlock)
+            stream().consumeAndMatch("{");
+        scopeManager().pushLoopScope(whileStatement);
+        var whileBody = new BlockParser(stream(), scopeManager(), whileStatement, hasBlock).parse();
+        scopeManager().popLoopScope();
+        if (hasBlock)
+            stream().consumeAndMatch("}");
+        return whileBody;
     }
 
     @Override
     public WhileStatement parse() {
         var whileStatement = new WhileStatement();
         whileStatement.setLabel(label);
-
         stream().consumeNextToken(); // "while"
-
         stream().consumeAndMatch("(");
-        var conditionExpression = new ExpressionParser(stream(), scopeManager()).parse();
+        whileStatement.setConditionExpression(new ExpressionParser(stream(), scopeManager()).parse());
         stream().consumeAndMatch(")");
-
-        whileStatement.setConditionExpression(conditionExpression);
-
-        var hasMultipleStatement = stream().peekNextToken().getValue().equals("{");
-
-        if (hasMultipleStatement)
-            stream().consumeAndMatch("{");
-
-        scopeManager().pushLoopScope(whileStatement);
-        var whileBody = new BlockParser(stream(), scopeManager(), whileStatement, hasMultipleStatement).parse();
-        scopeManager().popLoopScope();
-
-        if (hasMultipleStatement)
-            stream().consumeAndMatch("}");
-
-        whileStatement.setBody(whileBody);
+        whileStatement.setBody(parseBody(whileStatement));
         return whileStatement;
     }
 }
