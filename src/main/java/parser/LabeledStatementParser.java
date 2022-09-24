506bd978a9dbdede5b85514ffac075bc7f9e7106
devsh0 <devashishjaiswal86@gmail.com>
2022-09-24 12:28:35 +0530

    Allow LabeledStatement to be either `while` or `for`

--- src/main/java/parser/LabeledStatementParser.java
+++ src/main/java/parser/LabeledStatementParser.java
@@ -1,8 +1,10 @@
 package parser;
 
 import ast.Identifier;
-import ast.WhileStatement;
+import ast.Statement;
 import lexer.TokenStream;
+
+import static myutils.Macro.verify;
 
 public class LabeledStatementParser extends Parser {
     public LabeledStatementParser(TokenStream stream, ScopeManager scopeManager) {
@@ -10,10 +12,13 @@ public class LabeledStatementParser extends Parser {
     }
 
     @Override
-    public WhileStatement parse() {
+    public Statement parse() {
         var label = Identifier.from(stream().consumeIdentifier().getValue());
         stream().consumeAndMatch(":");
-        // FIXME: This can also be a switch or a for loop.
+        var tokenValue = stream().peekNextToken().getValue();
+        verify(tokenValue.equals("for") || tokenValue.equals("while"));
+        if (tokenValue.equals("for"))
+            return new ForStatementParser(stream(), scopeManager(), label).parse();
         return new WhileStatementParser(stream(), scopeManager(), label).parse();
     }
 }
