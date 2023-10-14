3b6d0835880c68bc6e7a939d8a5f1fbfe1342e05
devsh0 <devashishjaiswal@protonmail.com>
2023-10-15 00:23:43 +0530

    just add a test to ParserTest

--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -1,5 +1,23 @@
 package parser;
 
-public class ParserTest {
+import org.js.Interpreter;
+import org.junit.jupiter.api.Test;
 
+public class ParserTest {
+    @Test
+    void canExecuteJS() {
+        var interpreter = Interpreter.get();
+        var source = "let value = 10;\n" +
+                "\n" +
+                "function wrapper() {\n" +
+                "    let prevValue = value;\n" +
+                "    let value = 20;\n" +
+                "    prevValue + value;\n" +
+                "}\n" +
+                "\n" +
+                "wrapper();";
+        var program = Parser.parse(source);
+        var result = interpreter.run(program);
+        System.out.println(result.toString());
+    }
 }
